b550d026284b4c443e409d0db09f07767759b966
Xenia Shatova <98309459+Xenia-S@users.noreply.github.com>
2022-09-27 02:37:18 +0600

    Update ClosestQuakes.java

--- Week 1-1 SearchingEarthquakeDataStarterProgram/ClosestQuakes.java
+++ Week 1-1 SearchingEarthquakeDataStarterProgram/ClosestQuakes.java
@@ -1,11 +1,3 @@
-
-/**
- * Find N-closest quakes
- * 
- * @author Duke Software/Learn to Program
- * @version 1.0, November 2015
- */
-
 import java.util.*;
 
 public class ClosestQuakes {
@@ -28,9 +20,7 @@ public class ClosestQuakes {
 
     public void findClosestQuakes() {
         EarthQuakeParser parser = new EarthQuakeParser();
-        //String source = "data/nov20quakedata.atom";
         String source = "data/nov20quakedatasmall.atom";
-        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);
         System.out.println("read data for "+list.size());
 
