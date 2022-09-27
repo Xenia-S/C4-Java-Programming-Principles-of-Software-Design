bd72acb6ae1ac70356ea045c7d92b02ec470b346
Xenia Shatova <98309459+Xenia-S@users.noreply.github.com>
2022-09-27 08:05:24 +0600

    Update MagnitudeFilter.java

--- Week 1-2 EarthquakeFilterStarterProgram/MagnitudeFilter.java
+++ Week 1-2 EarthquakeFilterStarterProgram/MagnitudeFilter.java
@@ -1,10 +1,4 @@
 
-/**
- * Write a description of MagnitudeFilter here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MagnitudeFilter implements Filter {
     private double minMag;
     private double maxMag;
