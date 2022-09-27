552882924b8d92f193e99f5890d85c6b2f885005
Xenia Shatova <98309459+Xenia-S@users.noreply.github.com>
2022-09-27 08:09:15 +0600

    Update MagnitudeComparator.java

--- Week 2-2 EfficientSortStarterProgram/MagnitudeComparator.java
+++ Week 2-2 EfficientSortStarterProgram/MagnitudeComparator.java
@@ -1,11 +1,3 @@
-
-/**
- * Write a description of class MagnitudeComparator here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-
 import java.util.*;
 
 public class MagnitudeComparator implements Comparator<QuakeEntry> {
