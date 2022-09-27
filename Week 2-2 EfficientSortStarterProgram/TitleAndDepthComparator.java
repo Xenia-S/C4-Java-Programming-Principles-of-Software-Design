fa43f994cadcc92a44b182b8e2a868d4d0385809
Xenia Shatova <98309459+Xenia-S@users.noreply.github.com>
2022-09-27 08:09:36 +0600

    Update TitleAndDepthComparator.java

--- Week 2-2 EfficientSortStarterProgram/TitleAndDepthComparator.java
+++ Week 2-2 EfficientSortStarterProgram/TitleAndDepthComparator.java
@@ -1,11 +1,4 @@
 
-/**
- * Write a description of TitleAndDepthComparator here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-
 import java.util.*;
 
 public class TitleAndDepthComparator implements Comparator<QuakeEntry> {
