f28628a899f7ae5ae4d9a0d0e667a510242efe37
Xenia Shatova <98309459+Xenia-S@users.noreply.github.com>
2022-09-27 08:17:09 +0600

    Update MarkovFour.java

--- Week 3-2 InterfaceAbstractStarterProgram/MarkovFour.java
+++ Week 3-2 InterfaceAbstractStarterProgram/MarkovFour.java
@@ -21,24 +21,18 @@ public class MarkovFour extends AbstractMarkovModel {
         }
         StringBuilder sb = new StringBuilder();
         int index = myRandom.nextInt(myText.length()-4);
-        //System.out.println("Index:"+ index);
         String key = myText.substring(index, index+4);
-        //System.out.println("key:"+ key);
         sb.append(key);
         for(int k=0; k < numChars-4; k++){
             ArrayList<String> follows = getFollows(key);
-            //System.out.println(follows.toString());
             if(follows.size() == 0) { // если в конце текста myText стоят уникальные символы, метод getRandomText не сможет сгенерировать текст нужной длины.
                 key = sb.substring(0,4);
                 follows = getFollows(key);
             }
             index = myRandom.nextInt(follows.size());
             String next = follows.get(index);
-            //System.out.println("next index:"+ index + ", next:"+ next);
             sb.append(next);
-            //System.out.println("sb:"+ sb.toString());
             key = key.substring(1) + next;
-            //System.out.println("next key:"+ key);
         }
         
         return sb.toString();
